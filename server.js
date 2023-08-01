f1aabd2407b0d9a63237117690e84a2d23e8b536
shubhaseesh <shubhaseesh09@gmail.com>
2023-08-01 11:12:48 +0530

    fix task 1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.send("Hello, World!");
 });
 app.listen(PORT, () => {
